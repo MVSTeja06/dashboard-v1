c8c72e5c23161ff200a0cae0d17293d696b9a299
mvs teja <vmatc001@odu.edu>
2025-05-18 18:53:41 -0400

    Sign up form logic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,22 @@
-import { LoginForm } from "@/components/login-form"
-import { Suspense } from "react"
-import Loading from "./loading"
+"use client";
+
+import { LoginForm } from "@/components/login-form";
+import { Suspense, useState } from "react";
+import Loading from "./loading";
 
 export default function LoginPage() {
+  const [needsSignUp, setNeedsSignUp] = useState(false);
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <Suspense fallback={<Loading />}>
-          <LoginForm />
+          <LoginForm
+            setNeedsSignUp={setNeedsSignUp}
+            needsSignUp={needsSignUp}
+          />
         </Suspense>
       </div>
     </div>
-  )
+  );
 }
---
+++ components/ui/sign-up-form.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export const GoogleIcon = (props: object) => (
+  <svg role="img" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+    />
+  </svg>
+);
+
+interface SignUpFormProps extends React.ComponentProps<"div"> {
+  setNeedsSignUp: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function SignUpForm({ setNeedsSignUp }: SignUpFormProps) {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-2xl font-bold">Hello there!</h1>
+        <p className="text-muted-foreground text-balance">
+          Create your Account today!
+        </p>
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" placeholder="m@example.com" />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" type="password" />
+      </div>
+      <div className="text-center text-sm">
+        Have an account already?{" "}
+        <a
+          href="#"
+          className="underline underline-offset-4"
+          onClick={() => setNeedsSignUp(false)}
+        >
+          Login
+        </a>
+      </div>
+      <Button className="w-full">Create account</Button>
+    </div>
+  );
+}
